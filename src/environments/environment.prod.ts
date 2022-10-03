9d060994c58e877ae36715bd97c6c9b54d876991
Jinto Jose <jintoppy@gmail.com>
2022-10-03 21:49:02 +0200

    baseurl change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { firebaseConfig } from './firebase';
 
 export const environment = {
   production: true,
-  baseUrl: 'https://rainbow-florentine-bf5376.netlify.app',
+  baseUrl: 'https://delicate-froyo-2f95e5.netlify.app',
   firebase: firebaseConfig,
 };
